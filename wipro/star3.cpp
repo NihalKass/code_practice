96b3f8ba855f8196622eef6c0ca13a88e43e71aa
NihalKass <pappu.kr26@gmail.com>
2018-09-07 14:20:16 +0530

    star3

---
+++ wipro/star3.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{   
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        if(i==0 || i==n-1)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+        else
+        {
+            cout<<"*";
+            for(int k=0;k<n-2;k++)
+            {
+                cout<<" ";
+            }
+            cout<<"*";
+            cout<<endl;
+        }
+       
+    }
+    return 0;
+}
